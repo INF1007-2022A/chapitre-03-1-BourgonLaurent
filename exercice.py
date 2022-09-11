d4d0427fbb0d8ba7e6817862bac29c2d6239e2e8
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-09-11 13:55:21 -0400

    implement radians

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return math.radians(angle_degs + (angle_mins + angle_secs / 60) / 60)
 
 
 def to_degrees(angle_rads: float) -> tuple:
